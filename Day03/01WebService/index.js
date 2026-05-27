f64034e398869d1c36663410ca21a082a9d7554b
sukwon2000k <sukwon2000k@gmail.com>
2026-05-27 15:26:21 +0900

    Day03

---
+++ Day03/01WebService/index.js
@@ -0,0 +1,9 @@
+function aaa(){
+    var img= document.getElementById('aa')
+    img.src='./image/paris.jpg'
+}
+
+function bbb(){
+    var img= document.getElementById('aa')
+    img.src='./image/newyork.jpg'
+}
